bb698e4a59a1779bc4e7aaa0d9c53f23cad3cd71
Alexander Voronkov <alxn1101r@gmail.com>
2025-04-07 00:46:37 +0300

    Fixed building public module

--- build.zig
+++ build.zig
@@ -4,12 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("wav", .{
         .root_source_file = b.path("src/wav.zig"),
         .target = target,
         .optimize = optimize,
     });
-
 
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
@@ -27,7 +26,6 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
